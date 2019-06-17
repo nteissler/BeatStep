20fff35c1289a7b4ef2d638f924d19445cddd8a3
Nick Teissler <nick.teissler@affirm.com>
2019-06-17 07:10:05 -0700

    something to test

--- BeatStepTests/BeatStepTests.swift
+++ BeatStepTests/BeatStepTests.swift
@@ -18,13 +18,7 @@ class BeatStepTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("this is fine")
+        XCTAssert(true)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
